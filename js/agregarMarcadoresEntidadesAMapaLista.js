aa116f60399cf3ef14b7e15273a7ed0eb0c3b671
Pablo Arturo Perez Mata <pablo.arturo.perez@gmail.com>
2024-09-02 23:25:19 -0600

    Cambios finales

--- js/agregarMarcadoresEntidadesAMapaLista.js
+++ js/agregarMarcadoresEntidadesAMapaLista.js
@@ -1,13 +1,21 @@
-// Array to store markers
+// Array para almacenar los marcadores
 var markers = [];
 
-// Function to add markers to the map with custom icons and populate the dropdown
+// Función para agregar marcadores al mapa con íconos personalizados y poblar el dropdown
 function agregarMarcadoresEntidadesAMapa(csvConvertidoAObjeto, map) {
     const selector = document.getElementById('stateSelector');
-    
+
+    // Limpiar el dropdown antes de agregar opciones
+    selector.innerHTML = '';
+
+    // Agregar la opción inicial "Selecciona un estado"
+    const defaultOption = document.createElement('option');
+    defaultOption.value = ""; // Valor vacío para representar "ningún estado seleccionado"
+    defaultOption.text = "Selecciona un estado";
+    selector.appendChild(defaultOption);
+
     csvConvertidoAObjeto.forEach(row => {
         var entidad = row['PODER_EJECUTIVO_ENTIDAD'];
-        console.log('Procesando entidad:', entidad); // Añadir registro de cada entidad
 
         if (!entidad) {
             return;
@@ -18,18 +26,17 @@ function agregarMarcadoresEntidadesAMapa(csvConvertidoAObjeto, map) {
         option.value = entidad;
         option.text = entidad;
         selector.appendChild(option);
-        console.log('Añadida opción al selector:', entidad); // Confirmar adición al selector
 
         var partido = row['PODER_EJECUTIVO_PARTIDO'];
         var partidoIconUrl = `imagenes/partidos/${partido.toLowerCase().replace(/\s+/g, '')}.jpg`;
         var entidadIconUrl = `imagenes/estados/${entidad.toLowerCase().replace(/\s+/g, '')}.png`;
-        var normalIconSize = [50, 54];  // Ajustar tamaño del ícono
+        var normalIconSize = [50, 55]; // Hacer los iconos un poco más grandes
 
         var markerIcon = L.icon({
             iconUrl: entidadIconUrl,
             iconSize: normalIconSize,
-            iconAnchor: [25, 54], // Ajusta las coordenadas del anclaje del ícono
-            popupAnchor: [1, -36]
+            iconAnchor: [12, 41],
+            popupAnchor: [1, -34]
         });
 
         var marker = L.marker([parseFloat(row['LATITUD_DEL_ESTADO']), parseFloat(row['LONGITUD_DEL_ESTADO'])], { icon: markerIcon });
@@ -57,18 +64,44 @@ function agregarMarcadoresEntidadesAMapa(csvConvertidoAObjeto, map) {
 
     // Manejar el evento de cambio en el selector de estados
     document.getElementById('stateSelector').addEventListener('change', function() {
-        // Eliminar todos los marcadores existentes del mapa
-        markers.forEach(marker => map.removeLayer(marker));
-
         const selectedState = this.value;
+
+        // Si se selecciona "Selecciona un estado"
+        if (selectedState === "") {
+            // Limpiar el mapa de marcadores
+            markers.forEach(marker => map.removeLayer(marker));
+
+            map.setView([23.6345, -102.5528], 5); // Coordenadas y zoom para mostrar todo el país
+            map.closePopup(); // Cerrar cualquier popup abierto
+            clearStateInfo(); // Limpiar la información del estado seleccionado
+            return; // Salir de la función para evitar mostrar un marcador
+        }
+
         const marker = markers.find(marker => marker.estado === selectedState);
         if (marker) {
+            // Limpiar el mapa de cualquier marcador anterior antes de agregar el nuevo
+            markers.forEach(marker => map.removeLayer(marker));
+
             mostrarInformacionEstado(marker);
-            marker.addTo(map);  // Añadir el marcador seleccionado al mapa
-            marker.openPopup(); // Abre el popup manualmente al seleccionar el estado
-            map.setView(marker.getLatLng(), 8); // Centra el mapa en el marcador seleccionado
+            marker.addTo(map); // Asegurarse de que el marcador esté en el mapa
+            marker.openPopup(); // Abrir el popup manualmente al seleccionar el estado
+            map.setView(marker.getLatLng(), 7); // Centrar el mapa en el marcador seleccionado con un zoom más cercano
         }
     });
+}
+
+// Función para limpiar la información del estado seleccionado
+function clearStateInfo() {
+    document.getElementById('Titular').innerText = 'Titular Ejecutivo del Organismo: ';
+    document.getElementById('Presidente').innerText = 'Presidente del Organismo: ';
+    document.getElementById('leyEgresos').innerText = 'Presupuesto de Egresos: ';
+    document.getElementById('Ley_CTI').innerText = 'Ley CTI: ';
+    document.getElementById('Presupuesto_A').innerText = 'Presupuesto Asignado: ';
+    document.getElementById('Presupuesto').innerText = 'Porcentaje en Presupuesto en Tecnología: ';
+    document.getElementById('Innovacion').innerText = 'Innovacion: ';
+    document.getElementById('Acceso_Abierto').innerText = 'Acceso abierto: ';
+    document.getElementById('Vinculacion').innerText = 'Vinculacion: ';
+    document.getElementById('CTI_Genero').innerText = 'Genero: ';
 }
 
 // Función para mostrar la información del estado seleccionado
@@ -96,36 +129,4 @@ function mostrarInformacionEstado(marker) {
     document.getElementById('Acceso_Abierto').innerText = 'Acceso abierto: ' + row['Acceso_Abierto'];
     document.getElementById('Vinculacion').innerText = 'Vinculacion: ' + row['Vinculacion'];
     document.getElementById('CTI_Genero').innerText = 'Genero: ' + row['genero_CTI'];
-}
-
-// Manejar el evento de cambio en el selector de estados
-document.getElementById('stateSelector').addEventListener('change', function() {
-    const selectedState = this.value;
-
-    if (selectedState === "") {
-        // Desmarcar todo y limpiar la información
-        markers.forEach(marker => marker.closePopup());
-        limpiarInformacionEstado();
-        return;
-    }
-
-    const marker = markers.find(marker => marker.estado === selectedState);
-    if (marker) {
-        mostrarInformacionEstado(marker);
-        marker.openPopup(); // Abre el popup manualmente al seleccionar el estado
-        map.setView(marker.getLatLng(), map.getZoom()); // Centra el mapa en el marcador seleccionado
-    }
-});
-
-function limpiarInformacionEstado() {
-    document.getElementById('Titular').innerText = "Titular Ejecutivo del Organismo: ";
-    document.getElementById('Presidente').innerText = "Presidente del Organismo: ";
-    document.getElementById('leyEgresos').innerHTML = "Presupuesto de Egresos: ";
-    document.getElementById('Ley_CTI').innerText = "Ley CTI: ";
-    document.getElementById('Presupuesto_A').innerText = "Presupuesto Asignado: ";
-    document.getElementById('Presupuesto').innerText = "Porcentaje en Presupuesto en Tecnología: ";
-    document.getElementById('Innovacion').innerText = "Innovacion: ";
-    document.getElementById('Acceso_Abierto').innerText = "Acceso abierto: ";
-    document.getElementById('Vinculacion').innerText = "Vinculacion: ";
-    document.getElementById('CTI_Genero').innerText = "Genero: ";
 }
